67869fbfbdb92173aa0d878c53c880af7bfa7821
@rostovHomer <filipprnd161@gmail.com>
2022-11-21 00:03:00 +0300

    фикс имени хука

--- src/common/hooks/use-get-current-user.ts
+++ src/common/hooks/use-get-current-user.ts
@@ -3,7 +3,7 @@ import { useQuery } from 'react-query';
 import GetUsersService from "../../api/get-users-service";
 
 const useGetCurrentUser = (login: string) => {
-    const { isLoading, data, isError } = useQuery(['useGetUserLogin'], async () => {
+    const { isLoading, data, isError } = useQuery(['useGetCurrentUser'], async () => {
         return await GetUsersService.findCurrentUser(login);
     })
     return { isLoading, data, isError };
