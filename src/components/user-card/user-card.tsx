3590d423d2ddb073c7cd2968adb04a6bf376f3a0
@rostovHomer <filipprnd161@gmail.com>
2022-11-20 20:24:02 +0300

    правки в именовании

--- src/components/user-card/user-card.tsx
+++ src/components/user-card/user-card.tsx
@@ -11,16 +11,20 @@ export const UserCard = (props: IProps) => {
 
     const { isLoading, data } = useGetUsersId(search);
 
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+
     return (
         <>
-            {isLoading ? 'Loading...' : data?.items.map((el) => {
+            {data?.items.map((el) => {
                 return (
                     <div style={{ display: 'flex', width: 200, justifyContent: 'center', alignItems: 'center', flexDirection: 'column', border: '1px solid black', borderRadius: 25, margin: 10 }} key={el.id}>
                         <img style={{ width: 80, height: 80, paddingTop: 20, }} src={el.avatar_url
                         } alt="avatar" />
                         <p>{el.login}</p>
                         <a style={{ paddingBottom: 20 }} href={el.html_url}>{'Репозиторий'}</a>
-                        <Link to={`/user/${el.login}`}>Открыть в другом окне</Link>
+                        <Link style={{paddingBottom: 20}} to={`/user/${el.login}`}>Открыть в другом окне</Link>
                     </div>
 
                 )
--- src/core/navigation.tsx
+++ src/core/navigation.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { PageHome } from "../pages/page-home/page-home";
-import { PageCurrentUser } from "../pages/page-user/page-current-user";
+import { PageCurrentUser } from "../pages/page-current-user/page-current-user";
 import { NotFound } from '../pages/not-found';
 
 export const Navigation = () => {
